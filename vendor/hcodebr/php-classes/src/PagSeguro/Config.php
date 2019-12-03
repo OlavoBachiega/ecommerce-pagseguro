3bf437e3a1205cfc974f58a952a27307a4383353
olavobachiega@yahoo.com.br <OlavoBachiega>
2019-12-03 18:35:30 -0300

    Configuração inicial do projeto

---
+++ vendor/hcodebr/php-classes/src/PagSeguro/Config.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Hcode\PagSeguro;
+
+class Config {
+
+    const SANDBOX = true;
+
+    const SANDBOX_EMAIL = "olavobachiega@gmail.com";
+    const PRODUCTION_EMAIL = "olavobachiega@gmail.com";
+
+    const SANDBOX_TOKEN = "8AD82EBF03E949ABBF8A2793D6CC5761";
+    const PRODUCTION_TOKEN = "c0c4c8d3-d5c9-46e6-a74a-3c555a0ecedf1fb74cff4104b202dae7794d2d73f48be56e-58d5-4d8e-baff-215c98e701aa";
+
+    const SANDBOX_SESSIONS = "https://ws.sandbox.pagseguro.uol.com.br/sessions";
+    const PRODUCTION_SESSIONS = "https://ws.pagseguro.uol.com.br/sessions";
+
+    const SANDBOX_URL_JS = "https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+    const PRODUCTION_URL_JS = "https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+
+    public static function getAuthentication():array
+    {
+
+        if (Config::SANDBOX === true)
+        {
+
+            return [
+                "email"=>Config::SANDBOX_EMAIL,
+                "token"=>Config::SANDBOX_TOKEN
+            ];
+
+        } else {
+
+            return [
+                "email"=>Config::PRODUCTION_EMAIL,
+                "token"=>Config::PRODUCTION_TOKEN
+            ];
+
+        }
+
+    }
+
+    public static function getUrlSessions():string
+    {
+
+        return (Config::SANDBOX === true) ? Config::SANDBOX_SESSIONS : Config::PRODUCTION_SESSIONS;
+        
+    }
+
+    public static function getUrlJS()
+    {
+
+        return (Config::SANDBOX === true) ? Config::SANDBOX_URL_JS : Config::PRODUCTION_URL_JS;
+
+    }
+
+}
+
+?>
