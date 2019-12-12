f49401b77feec84ba37a0313547e978ceda98d79
olavobachiega@yahoo.com.br <OlavoBachiega>
2019-12-12 16:50:55 -0300

    Classe Shipping

--- vendor/hcodebr/php-classes/src/PagSeguro/Shipping.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Shipping.php
@@ -2,6 +2,10 @@
 
 namespace Hcode\PagSeguro;
 
+use DOMDocument;
+use DOMElement;
+use Exception;
+
 class Shipping {
 
     const PAC = 1;
@@ -12,5 +16,55 @@ class Shipping {
     private $type;
     private $cost;
     private $addressRequired;
+
+    public function __construct(
+        Address $address,
+        float $cost,
+        int $type,
+        bool $addressRequired
+    )
+    {
+        
+        if ($type < 1 || $type > 3)
+        {
+
+            throw new Exception("Informe um tipo de frete válido.");
+
+        }
+
+        $this->address = $address;
+        $this->cost = $cost;
+        $this->type = $type;
+        $this->addressRequired = $addressRequired;
+
+    }
+
+    public function getDOMElement():DOMElement
+    {
+
+        $dom = new DOMDocument();
+
+        $shipping = $dom->createElement("shipping");
+        $shipping = $dom->appendChild($shipping);
+
+        $documents = $dom->createElement("documents", $this->documents);
+        $documents = $shipping->appendChild($documents);
+
+        $address = $this->address->getDOMElement();
+        $address = $dom->importNode($address, true);
+        $address = $documents->appendChild($address);
+
+        $cost = $dom->createElement("cost", number_format($this->cost, 2, ".", ""));
+        $cost = $shipping->appendChild($cost);
+
+        $type = $dom->createElement("type", $this->type);
+        $type = $shipping->appendChild($type);
+
+        $addressRequerid = $dom->createElement("addressRequerid", ($this->addressRequerid ? "true" : "false"));
+        $addressRequerid = $shipping->appendChild($addressRequerid);
+
+        return $shipping;
+
+    }
 
 }
